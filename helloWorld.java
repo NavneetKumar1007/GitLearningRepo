e8891438d149ecbba303ef51ea4032b28922486c
Navneet Kumar <navneet.kumar1007@gmail.com>
2020-05-31 23:27:57 +0530

    intitial Commit

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class helloWorld
+{
+  public static void main(String args[])
+   {
+      System.out.println("Hello world");
+   }
+}
